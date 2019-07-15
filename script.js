9df44596bd37e23bd30b86c09b50b8283a7b9e88
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-15 11:40:35 +0200

    made api call

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('searchBtn').addEventListener('click', searchPokemon);
 
 function searchPokemon() {
-    let userInput = document.getElementById('inputField').value;
-    console.log(userInput);
-    
-    fetch(`https://pokeapi.co/api/v2/${userInput}/`)
+    let userInput = document.getElementById('inputField').value;    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}/`)
     .then(res => res.json())
     .then(data => console.log(data));
 }
