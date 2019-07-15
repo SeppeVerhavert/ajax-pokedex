b02225eab28d036f49b49a872bd5ba4100bba111
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-15 15:59:39 +0200

    add moveset

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 document.getElementById('searchBtn').addEventListener('click', searchPokemon);
 
 function searchPokemon() {
-    let userInput = document.getElementById('inputField').value;    
+    let userInput = document.getElementById('inputField').value;
     fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}/`)
-    .then(res => res.json())
-    .then(result => {
-        showData(result);
-    });
+        .then(res => res.json())
+        .then(result => {
+            showData(result);
+        });
 }
 
 function showData(serverData) {
+    showName(serverData);
+    showId(serverData);
+    showSprite(serverData);
+    showMoves(serverData);
+}
+
+function showName(serverData) {
     let nameHeader = document.getElementById("nameHeader");
     nameHeader.innerHTML = serverData.name;
-    
+}
+
+function showId(serverData) {
     let idHeader = document.getElementById("idHeader");
     idHeader.innerHTML = serverData.id;
+}
 
+function showSprite(serverData) {
     let sprite = document.getElementById("sprite");
     sprite.src = serverData.sprites.front_default;
+}
+
+function showMoves(serverData) {
+    let i = 0;
+    let moveSet = [];
+
+    for (i = 0; i != 4; i += 1) {
+        moveSet.push(serverData.moves[i].move.name);
+        document.getElementsByClassName("move")[i].innerHTML = moveSet[i];
+    }
 }
