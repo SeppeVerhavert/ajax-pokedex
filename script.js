e19011d7bddcea809b6ef0ff257a40dcc1b0f448
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-15 11:49:39 +0200

    add pokemon sprite

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function searchPokemon() {
     let userInput = document.getElementById('inputField').value;    
     fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}/`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(result => {
+        showData(result);
+    });
+}
+
+function showData(serverData) {
+    let nameHeader = document.getElementById("nameHeader");
+    nameHeader.innerHTML = serverData.name;
+    
+    let sprite = document.getElementById("sprite");
+    sprite.src = serverData.sprites.front_default;
 }
